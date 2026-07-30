3e7bc547756b12b9f82ae3abad2c9534f2ebb67a
githiago-f <tfarias@protonmail.com>
2026-07-29 23:41:46 -0300

    better configuration for plantuml and syntax support

--- config/nvim/lua/plugins/plantuml.lua
+++ config/nvim/lua/plugins/plantuml.lua
@@ -1,15 +1,17 @@
 vim.pack.add({ "https://github.com/githiago-f/plantuml.nvim" })
 vim.pack.add({ "https://github.com/aklt/plantuml-syntax" })
 
+vim.cmd.packadd("plantuml-syntax")
+
 require("plantuml").setup({
-  output = { format = "png", float = false },
-  cmd = {
-    exec = "plantuml",
-    debounce_ms = 2500,
-    temp_dir = "/tmp/nvim-puml",
-  },
+	output = { format = "png", float = false },
+	cmd = {
+		exec = "plantuml",
+		debounce_ms = 2500,
+		temp_dir = "/tmp/nvim-puml",
+	},
 })
 
 vim.keymap.set("n", "<leader>puml", "<cmd>PlantumlPreviewToggle<CR>", {
-  desc = "Toggle PlantUML preview",
+	desc = "Toggle PlantUML preview",
 })
